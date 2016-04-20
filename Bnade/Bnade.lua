cb1508d0401af52835f616943618e9bc71a9cc6c
Feng Liu <liufeng0103@163.com>
2016-04-20 13:25:15 +0800

    第一版可用
    1. 打印数据更新时间
    2. 鼠标移动到物品上时，显示物品的参考价格

---
+++ Bnade/Bnade.lua
@@ -0,0 +1,35 @@
+local GOLD_ICON = "|TInterface\\MoneyFrame\\UI-GoldIcon:0|t"
+local SILVER_ICON = "|TInterface\\MoneyFrame\\UI-SilverIcon:0|t"
+local COPPER_ICON = "|TInterface\\MoneyFrame\\UI-CopperIcon:0|t"
+
+local function MoneyToString(money)
+	money = tonumber(money)
+	local gold = floor(money / COPPER_PER_GOLD)
+	local silver = floor((money % COPPER_PER_GOLD) / COPPER_PER_SILVER)
+	local copper = floor(money % COPPER_PER_SILVER)
+	local text = nil
+	if gold > 0 then
+		text = gold .. GOLD_ICON .. silver .. SILVER_ICON .. copper .. COPPER_ICON
+	elseif silver > 0 then
+		text = silver .. SILVER_ICON .. copper .. COPPER_ICON
+	else 
+		text = copper .. COPPER_ICON
+	end
+	return text
+end
+local function OnTooltipSetItem(self)
+	local name, link = self:GetItem()
+	if name then		
+		local itemId = link:match("Hitem:(%d+)")
+		local price = Bnade_data[tonumber(itemId)]
+		if price then
+			GameTooltip:AddLine("BNADE参考价格: " .. MoneyToString(price), 1, 1, 1)
+		end			
+	end	
+end
+GameTooltip:HookScript("OnTooltipSetItem", OnTooltipSetItem)
+local updated = Bnade_data["updated"]
+if updated then
+	print("BNADE查看物品参考价格，数据更新时间: " .. updated)
+end
+
